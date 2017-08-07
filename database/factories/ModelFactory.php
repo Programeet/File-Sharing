241228fbf151fee73983ca5ce26619d4e2f0ae9e
Esteban D. 'K1d' Barboza Primera <dario0117@gmail.com>
2017-08-07 17:42:33 -0500

    Creadas las Factorías  para cada modelo

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,32 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
+$factory->define(App\Usuario::class, function (Faker\Generator $faker) {
 
     return [
-        'name' => $faker->name,
+        'nick' => $faker->firstNameMale(),
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'password' => str_random(20),
+        'tipo' => $faker->numberBetween($min = 1, $max = 2),
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Genero::class, function (Faker\Generator $faker) {
+    return [
+        'nombre' => $faker->company,
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Archivo::class, function (Faker\Generator $faker) {
+    return [
+        'nombre' => $faker->name,
+        'artista' => $faker->name,
+        'fecha_subida' => $faker->dateTime($max = 'now'),
+        'numero_descargas' => $faker->numberBetween($min = 1, $max = 2000),
+        'size' => $faker->numberBetween($min = 1024, $max = 2048),
+        'genero_id' => $faker->numberBetween($min = 1, $max = 12),
+        'usuario_id' => $faker->numberBetween($min = 1, $max = 12),
     ];
 });
